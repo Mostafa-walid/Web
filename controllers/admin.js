283af998d71713338e1565206910bc99e4f929c5
Hussein mostafa <hussein2203920@miuegypt.edu.eg>
2025-01-18 22:15:11 +0200

    Update admin.js
    dol el update user w el order page

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,3 +23,45 @@ const editUserPage = async (req, res) => {
 		res.status(500).send("Server error");
 	}
 };
+const updateUser = async (req, res) => {
+	const { role, username, email, password } = req.body;
+	const { targetUserId } = req.params;
+
+	try {
+		const user = await User.findById(targetUserId);
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		user.role = role;
+		user.username = username;
+		user.email = email;
+
+		if (password) {
+			user.password = password;
+		}
+
+		await user.save();
+		res.status(200).json({ message: "User updated successfully!" });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "An error occurred while updating the user" });
+	}
+};
+
+const orderPage = async (req, res) => {
+	try {
+		const usersWithPurchases = await User.find({
+			"purchases.0": { $exists: true },
+		})
+			.populate("purchases.carId", "type model price")
+			.select("name email purchases");
+		res.render("admin/orders", {
+			user: await User.findById(req.params.userId),
+			usersWithPurchases,
+		});
+	} catch (error) {
+		console.error("Error fetching orders:", error);
+		res.status(500).send("Server error");
+	}
+};
