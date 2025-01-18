34ddf47b694ef757935c27664612b43ba5ecacef
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 22:16:52 +0200

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -146,3 +146,77 @@ const updatePassword = async (req, res) => {
 		{ password: hashedPassword },
 		{ new: true, runValidators: true }
 	  );
+
+	  res.render("profile", { user: updatedUser });
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).send("Server error");
+	}
+  };
+  
+// Get User Profile with Order History
+const getUserProfile = async (req, res) => {
+	try {
+		const { userId } = req.params;
+
+		// Fetch user and populate purchases
+		const user = await User.findById(userId).populate("purchases.carId");
+
+		if (!user) {
+			return res.status(404).send("User not found");
+		}
+
+		res.render("profile", { user });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Server error");
+	}
+};
+
+const purchaseCar = async (req, res) => {
+	const userId = req.params.id;
+	const { purchaseDetails } = req.body;
+
+	const car = await Car.findById(purchaseDetails.carId);
+	if (car.quantity == 0)
+		return res
+			.status(500)
+			.json({ errMsg: "Car out of stock .. try again later" });
+	else car.quantity--;
+	car.save();
+
+	try {
+		const user = await User.findById(userId);
+		console.log(user);
+		if (!user.purchases) user.purchases = [];
+		user.purchases.push(purchaseDetails);
+		user.save();
+		res.status(200).json({ user });
+	} catch (err) {
+		res.status(500).json({ errMsg: err.message });
+	}
+};
+
+const deleteUser = async (req, res) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id))
+		return res.status(404).send(`No user with id: ${id}`);
+
+	await User.findByIdAndDelete(id);
+
+	res.json({ message: "User deleted successfully" });
+};
+
+export {
+	createUser,
+	getUsers,
+	getUserById,
+	updateUser,
+	purchaseCar,
+	deleteUser,
+	getUserProfile,
+	updatePassword,
+	getOrderHistory,
+	deletePurchase,
+};
