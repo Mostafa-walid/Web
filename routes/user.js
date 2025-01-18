a7e2029c82ad7880dea572ee5aa2e6305f1a7a4b
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 22:37:01 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,27 @@
+import express from "express";
+import {
+	createUser,
+	getUsers,
+	getUserById,
+	updateUser,
+	purchaseCar,
+	deleteUser,
+	updatePassword,
+	getOrderHistory,
+	deletePurchase,
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.post("/:id/update", updateUser);
+router.post("/:id/update-password", updatePassword);
+router.get("/order-history/:id", getOrderHistory);
+router.delete("/order-history/:id/:purchaseId", deletePurchase);
+router.put("/:id/purchase", purchaseCar);
+router.delete("/:id", deleteUser);
+
+export default router;
 
