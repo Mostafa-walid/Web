a7ac9b4eba26f8c91fa0bc84661ddf5fc5c8ab3b
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 22:16:01 +0200

    Update User.js

--- controllers/User.js
+++ controllers/User.js
@@ -49,3 +49,59 @@ const deletePurchase = async (req, res) => {
     } else {
       console.warn("Car associated with the purchase not found.");
     }
+ user.purchases.splice(purchaseIndex, 1);
+    await user.save();
+
+    res.status(200).send("Purchase deleted and car quantity updated successfully.");
+  } catch (error) {
+    console.error("Error deleting purchase:", error);
+    res.status(500).send("Server error");
+  }
+  };
+
+const getOrderHistory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find the user and populate purchase details
+    const user = await User.findById(id).populate("purchases.carId");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ purchases: user.purchases });
+  } catch (error) {
+    console.error("Error fetching order history:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getUserById = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const user = await User.findById(id);
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		res.render("index", { user, cars: await Car.find() });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
+const updateUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const updates = req.body;
+
+		const updatedUser = await User.findByIdAndUpdate(id, updates, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!updatedUser) {
+			return res.status(404).json({ errMsg: "User not found" });
+		}
