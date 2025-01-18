777de50169ef9b2434dcecafef67a2399cf769ca
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 22:18:01 +0200

    Create Cart.js

---
+++ controllers/Cart.js
@@ -0,0 +1,55 @@
+import User from "../models/User.js";
+
+export const addItemToCart = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const { carId, quantity } = req.body;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ errMsg: "User not found" });
+    }
+
+    const cartItem = user.cart.find(
+      (item) => item.carId.toString() === carId
+    );
+
+    if (cartItem) {
+      cartItem.quantity += quantity;
+    } else {
+      user.cart.push({ carId, quantity });
+    }
+
+    await user.save();
+    res.status(200).json(user.cart);
+  } catch (error) {
+    res.status(500).json({ errMsg: error.message });
+  }
+};
+
+export const removeItemFromCart = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const { carId } = req.body;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ errMsg: "User not found" });
+    }
+
+    user.cart = user.cart.filter(
+      (item) => item.carId.toString() !== carId
+    );
+    await user.save();
+    res.status(200).json(user.cart);
+  } catch (error) {
+    res.status(500).json({ errMsg: error.message });
+  }
+};
+
+export const updateItemQuantityInCart = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const { carId, quantity } = req.body;
+    const user = await User.findById(userId);
