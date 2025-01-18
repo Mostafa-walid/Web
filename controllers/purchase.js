16e9a11dc4e442efa78c34d9929b2614188652cb
Hussein mostafa <hussein2203920@miuegypt.edu.eg>
2025-01-18 22:19:12 +0200

    Create purchase.js

---
+++ controllers/purchase.js
@@ -0,0 +1,85 @@
+import Purchase from "../models/Purchase.js";
+import Car from "../models/Car.js";
+import User from "../models/User.js";
+import mongoose from "mongoose";
+
+const createPurchase = async (req, res) => {
+  const userId = req.params.userId;
+  const { amountPaid, purchaseType } = req.body;
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const cart = user.cart;
+
+  if (!cart || cart.length === 0) {
+    return res.status(400).json({ errMsg: "Cart is empty" });
+  }
+
+  let totalPrice = 0;
+
+  try {
+    
+    for (const item of cart) {
+      const car = await Car.findById(item.carId);
+      if (!car || car.quantity < item.quantity) {
+        return res
+          .status(400)
+          .json({ error: `Not enough quantity for car ${car.model}` });
+      } else {
+        totalPrice += car.price * item.quantity;
+      }
+    }
+
+   
+    const purchase = new Purchase({
+      userId,
+      cart,
+      totalPrice,
+      amountPaid,
+      purchaseType,
+    });
+
+    // Update car quantities
+    for (const item of cart) {
+      const car = await Car.findById(item.carId);
+      car.quantity -= item.quantity;
+      await car.save();
+    }
+
+   
+    user.cart = [];
+    await user.save();
+
+    await purchase.save();
+    return res.status(201).json({ purchase });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errMsg: "Internal server error" });
+  }
+};
+
+const getPurchases = async (req, res) => {
+  try {
+    const purchases = await Purchase.find();
+    res.status(200).json(purchases);
+  } catch (error) {
+    res.status(404).json({ errMsg: error.message });
+  }
+};
+
+const getPurchaseById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const purchase = await Purchase.findById(id);
+    if (!purchase) {
+      return res.status(404).json({ message: "Purchase not found" });
+    }
+    res.status(200).json(purchase);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
