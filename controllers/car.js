7497a35cfe53e2129dbda7574090b626845f0d37
aabdelrahmanssalah <abdelrahman2204989@miuegypt.edu.eg>
2025-01-18 22:16:06 +0200

    Update car.js
    khlst el file path wel createcar wel imageupload

--- controllers/car.js
+++ controllers/car.js
@@ -4,3 +4,21 @@ import mongoose from "mongoose";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createCar = async (req, res) => {
+  const carData = req.body;
+  try {
+    const newCar = new Car(carData);
+    await newCar.save();
+    res.json({ message: "Car added successfully" });
+  } catch (error) {
+    res.status(500).json({ errMsg: error.message });
+  }
+};
+
+const uploadImage = async (req, res) => {
+  const imageUrl = "/img/" + req.file.filename;
+  res.json({ imageUrl });
+};
