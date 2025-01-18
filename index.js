1bffb428edcc82798923825400421ab4430f5cdb
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 21:43:30 +0200

    Update index.js
    add lel imports

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+const app = express();
+import secrets from "./config/secrets.js";
+const { port } = secrets;
+import connectDB from "./config/db.js";
+
+//routers
+import registrationRoutes from './routes/registration.js';
+import userRoutes from './routes/user.js';
+import carRoutes from './routes/car.js';
+import purchaseRoutes from './routes/purchase.js';
 
