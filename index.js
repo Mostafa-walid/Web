ffb05ba747e4d645a5d65128363167920240804d
Mostafa-walid <mostafa2206331@miuegypt.edu.eg>
2025-01-18 21:44:02 +0200

    Update index.js
    finishing el index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+ import express from "express";
 import cookieParser from "cookie-parser";
 const app = express();
 import secrets from "./config/secrets.js";
@@ -11,3 +11,19 @@ import userRoutes from './routes/user.js';
 import carRoutes from './routes/car.js';
 import purchaseRoutes from './routes/purchase.js';
 
+app.use(express.static("public")); // to read static files (css, js, img)
+app.use(express.json()); // to read req.body
+app.use(express.urlencoded({ extended: true })); // to read req.body
+app.use(cookieParser());
+app.set("view engine", "ejs"); // to set view engine to ejs
+
+
+app.use('/', registrationRoutes);
+app.use('/users', userRoutes);
+app.use('/cars', carRoutes);
+app.use('/purchases', purchaseRoutes);
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Example app listening on port ${port}`);
+});
